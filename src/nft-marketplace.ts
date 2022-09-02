2209c13486ee40c1a8714f4b323dd80248ce74e6
maikelordaz <maikelordaz@gmail.com>
2022-09-02 12:47:27 -0400

    Final touches and checks

--- src/nft-marketplace.ts
+++ src/nft-marketplace.ts
@@ -1,34 +1,15 @@
-// tipos de variables
 import { Address, BigInt } from "@graphprotocol/graph-ts";
 import {
-  // Escribo as Item***** para que sea un poco mas facil de leer despues
-  ItemBought as ItemBoughtEvent,
+   ItemBought as ItemBoughtEvent,
   ItemCanceled as ItemCanceledEvent,
   ItemListed as ItemListedEvent
 } from "../generated/NFTMarketplace/NFTMarketplace"
 import {ActiveItem, ItemListed, ItemBought, ItemCanceled} from "../generated/schema"
 
 export function handleItemBought(event: ItemBoughtEvent): void {
-  /*
-  * Cada vez que listee un item necesito guardarlo en el graph
-  * Actualizar ActiveItem
-  * Tomar o crear el itemListed object
-  * Cada item tiene un ID unico
-  * 
-  * ItemBoughtEvent -> el evento
-  * ItemBoughtObject: Lo que guardo
-  * 
-  * ItemBoughtObject y ItemBoughtEvent son tipos distintos y son auto creados en schema y hay 
-  * que importarlos
-  * 
-  * Necesito crear un ItemBoughtObject del ItemBought Event
-  */
-
-  // Con .load cargo el ID unico segun los parametros dados
   let itemBought = ItemBought.load(
     getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
     )
-  // No importa que sea el mismo ID porque esta en tipos distintos. 
   let activeItem = ActiveItem.load(
     getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
   )
@@ -37,11 +18,10 @@ export function handleItemBought(event: ItemBoughtEvent): void {
       getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
     )
   }
-  // Actualizo cada parametro de ItemBought
+
   itemBought.buyer = event.params.buyer
   itemBought.nftAddress = event.params.nftAddress
   itemBought.tokenId = event.params.tokenId
-  // El ! significa que tendremos un activeItem. Si hay un comprador significa que lo compraron
   activeItem!.buyer = event.params.buyer
 
   itemBought.save()
@@ -100,14 +80,11 @@ let activeItem = ActiveItem.load(
   itemListed.price = event.params.price
   activeItem.price = event.params.price
 
-  // Si tiene la direccion 0 quiere decir que esta en el mercado y no ha sido comprado
-  activeItem.buyer = Address.fromString("0x0000000000000000000000000000000000000000")
+    activeItem.buyer = Address.fromString("0x0000000000000000000000000000000000000000")
   
   itemListed.save()
   activeItem.save()
 }
-
-//function nombreFuncion(parametro: tipo de parametro): tipoDeRetorno {}
 
 function getIdFromEventParams(tokenId: BigInt, nftAddress: Address): string {
   return tokenId.toHexString() + nftAddress.toHexString()
